d50693924e278f788e6d1657d4e1d972d533a541
m3l1k4 <melikaihelas@gmail.com>
2021-03-14 17:02:09 -0700

    cleanup

--- challenge/backend/server.js
+++ challenge/backend/server.js
@@ -2,15 +2,22 @@ const express = require ('express')
 const app = express()
 const port = process.env.PORT || process.argv[2] || 8080
 const bodyParser = require ('body-parser')
-
+const cors = require('cors')
 const vehiclesList = require('./API/vehicles.json')
 
 app.use(bodyParser.json());
-
+app.use(cors());
 
 app.get('/vehicles', (req,res) => {
     res.json(vehiclesList)
 })
+
+
+app.use('/login', (req, res) => {
+    res.send({
+      token: 'test123'
+    });
+  });
 
 
 app.listen(port, ()=> console.log(`Listetning on ${port}`))
